ff62aa0e784e9f7509cd51c98e2a57d4d01c8a2a
Mikask <88318140+Mikask1@users.noreply.github.com>
2022-12-13 00:07:28 +0700

    feat: added charging enemy (#15)

--- src/character/monster/Monster.java
+++ src/character/monster/Monster.java
@@ -1,14 +1,15 @@
 package character.monster;
 
 import java.awt.Image;
+import java.awt.image.BufferedImage;
 
 import character.Character;
 import main.GamePanel;
 
 public abstract class Monster extends Character{
-	public Image monLeft, monLeft1, monLeft2, monLeft3, monLeft4, monRight, monRight1, monRight2, monRight3,
+	public BufferedImage monLeft, monLeft1, monLeft2, monLeft3, monLeft4, monRight, monRight1, monRight2, monRight3,
 	monRight4;
-	public Image hurtLeft, hurtLeft1, hurtLeft2, hurtLeft3, hurtLeft4, hurtRight, hurtRight1, hurtRight2, hurtRight3,
+	public BufferedImage hurtLeft, hurtLeft1, hurtLeft2, hurtLeft3, hurtLeft4, hurtRight, hurtRight1, hurtRight2, hurtRight3,
 	hurtRight4;
 	
 	public Monster(GamePanel gp) {
